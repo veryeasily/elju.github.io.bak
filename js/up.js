7aabb867b5e9916230315f6305208c6753e65529
elju <moresilenter@gmail.com>
2013-05-15 13:20:11 -0500

    gah

--- js/up.js
+++ js/up.js
@@ -1,6 +1,7 @@
 $(document.body).on('click', function(e) {
     var testObj = new Object();
     testObj.something = 'yo!';
+    window.parent.postMessage('click', 'http://lju.me');
     window.parent.postMessage('click', 'http://localhost:5000');
     console.log("click detected!");
 });
