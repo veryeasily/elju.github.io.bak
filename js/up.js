b2f81465b64a50fdd127c57eb84ad943db1d403c
elju <moresilenter@gmail.com>
2013-05-15 13:05:23 -0500

    switched the message posted

--- js/up.js
+++ js/up.js
@@ -1,6 +1,6 @@
 $(document.body).on('click', function(e) {
     var testObj = new Object();
     testObj.something = 'yo!';
-    window.parent.postMessage(testObj, 'http://localhost:5000');
+    window.parent.postMessage('click', 'http://localhost:5000');
     console.log("click detected!");
 });
