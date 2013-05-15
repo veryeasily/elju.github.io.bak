ecf1ef9e8366376e6f741d3f07fc6fcbcb43e689
elju <moresilenter@gmail.com>
2013-05-15 12:39:18 -0500

    figuring out how targetOrigin works

--- js/up.js
+++ js/up.js
@@ -1,4 +1,4 @@
 $(document.body).on('click', function(e) {
-    window.parent.postMessage('clicked', 'http://elju.github.io');
+    window.parent.postMessage('clicked', 'http://localhost:5000');
     console.log("click detected!");
 });
