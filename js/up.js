789994cea2339543def0ca03d07cf7f390316130
elju <moresilenter@gmail.com>
2013-05-15 12:36:46 -0500

    changed stuff

--- js/up.js
+++ js/up.js
@@ -1,4 +1,4 @@
 $(document.body).on('click', function(e) {
-    window.parent.postMessage('clicked', '*');
+    window.parent.postMessage('clicked', 'http://elju.github.io');
     console.log("click detected!");
 });
